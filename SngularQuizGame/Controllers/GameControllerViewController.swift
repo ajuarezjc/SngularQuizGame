65ed7386b0bcf720d51b75c31ec28d702692b0bc
ALEJANDRO_JUAREZ <AJUAREZ@VCROBOTICA.COM>
2021-05-20 22:30:10 -0500

    Adding Game functionality to GameController

--- SngularQuizGame/Controllers/GameControllerViewController.swift
+++ SngularQuizGame/Controllers/GameControllerViewController.swift
@@ -7,12 +7,79 @@
 
 import UIKit
 
+enum AnswerSelected : Int {
+    case A = 0
+    case B = 1
+}
+
 class GameControllerViewController: UIViewController {
+    
+    /*IB Outlets*/
+    @IBOutlet weak var labelQuestionText: UILabel!
+    @IBOutlet weak var buttonOptionA: UIButton!
+    @IBOutlet weak var buttonOptionB: UIButton!
+    
+    /*Controller Variables*/
+    var game : GameModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //Load the questions and set a new game
+        //This could/should be done on GameModel.swift too
+        if let questionsForGame = getQuestions(){
+            //Init the game
+            game = GameModel(withQuestions: questionsForGame)
+            game.shuffleQuestions()
+        }
+        
+        //Setting tags for buttons for easy answer check
+        buttonOptionA.tag = AnswerSelected.A.rawValue
+        buttonOptionB.tag = AnswerSelected.B.rawValue
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //Set the initial question and options
+        
+    }
+    
+    // MARK: - Controller Functions
+    
+    //Load questions
+    func getQuestions()->[QuestionModel]?{
+        if let path = Bundle.main.path(forResource: "Questions", ofType: "json"){
+            do{
+                let data = try Data(contentsOf: URL(fileURLWithPath: path))
+                let questions = try JSONDecoder().decode([QuestionModel].self, from: data)
+                return questions
+            } catch let err {
+                //Something wrong happened, return nil
+                print("Error while getting questions:\n"+err.localizedDescription)
+                return nil
+            }
+        } else{
+            return nil
+        }
+        
+    }
+    
+    func updateQuestionInView(advanceQuestion goToNextQuestion : Bool = false){
+        //Check if we have to go to next question
+        if goToNextQuestion {
+            game.goToNextQuestion()
+        }
+        //Display current question in View
+        //Question
+        labelQuestionText.text = game.currentQuestion.Question
+        //Option A
+        buttonOptionA.setTitle(game.currentQuestion.OptionA, for: .normal)
+        //Option B
+        buttonOptionB.setTitle(game.currentQuestion.OptionB, for: .normal)
+    }
+    
+    
+    // MARK: - IB Actions
+    
 
 }
--- SngularQuizGame/Models/GameModel.swift
+++ SngularQuizGame/Models/GameModel.swift
@@ -8,27 +8,42 @@
 import Foundation
 
 struct GameModel {
-    var rightGuesses : Int = 0
-    var wrongGuesses : Int = 0
-    var currentQuestion : Int = 0
+    private var rightGuesses : Int = 0
+    private var wrongGuesses : Int = 0
+    private var currentQuestionIndex : Int = 0
     
-    var allQuestions : [QuestionModel]
+    private var allQuestions : [QuestionModel]
     var totalQuestions : Int {
         return allQuestions.count
+    }
+    
+    var currentQuestion : QuestionModel {
+        return allQuestions[currentQuestionIndex]
+    }
+    
+    init(withQuestions questionsForGame : [QuestionModel]) {
+        self.rightGuesses = 0
+        self.wrongGuesses = 0
+        self.currentQuestionIndex = 0
+        self.allQuestions = questionsForGame
     }
     
     mutating func resetGame(){
         self.rightGuesses = 0
         self.wrongGuesses = 0
-        self.currentQuestion = 0
+        self.currentQuestionIndex = 0
     }
+    
+    mutating func shuffleQuestions(){
+        allQuestions.shuffle()
+    }
+    
+    mutating func goToNextQuestion(){
+        self.currentQuestionIndex += 1
+    }
+    
 
-    init(withQuestions questionsForGame : [QuestionModel]) {
-        self.rightGuesses = 0
-        self.wrongGuesses = 0
-        self.currentQuestion = 0
-        self.allQuestions = questionsForGame
-    }
+    
 }
 
 
